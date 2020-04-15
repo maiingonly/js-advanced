bd217d2a8060ab39724e25adafc40f50dfa1b322
maiingonly <maherboleax@gmail.com>
2020-04-15 23:25:36 +0700

    menyelesaikan seri pa dika 10.2 callback(latian) dan menambahkan todo di Readme.md)

---
+++ 10.2 callback(latihan)/Readme.md
@@ -0,0 +1,29 @@
+  >>>>>>>>> Java script
+# Deleng Movie - Movie search
+    a basic Database to get information movie
+    asynchronous callback with JQuery practice 
+
+## To Do
+* [x] create key and test OMDb API in Postman(optional)
+* [x] setup index.html with starter template bootstrap
+    * [x] bootstrap (get JQuery-minified and change it)
+* [x] create header
+* [x] build placeholder box for movie
+    * [x] create container , row , column 'col-md-4 my-3'
+    * [x] card 
+       * https://getbootstrap.com/docs/4.4/components/card/
+    
+* [x] create JQuery
+    * [x] Get data from API
+    * [x] looping results API to placeholder
+      
+* [] button Show Details request different ajax
+    * [x] search and create Modal box
+        * + vertically centered = modal-dialog-centered
+        * + optional size = modal-lg
+    * [x] copy data-togel and data-target(should same with id button)
+    * [x] replace to button loop
+* [x] create container-fluid in modal body
+    * list-group
+* [] request ajax while send data imdbID 
+[]  
--- 10.2 callback(latihan)/main.js
+++ 10.2 callback(latihan)/main.js
@@ -1,24 +1,69 @@
-
-$.ajax({
-    url: 'http://www.omdbapi.com/?apikey=c4b6762c&s=hack',
+$('.searchBtn').on('click', function () {
+    $.ajax({
+    url: 'http://www.omdbapi.com/?apikey=c4b6762c&s=' + $('.input-keyword').val(),
     success: results => {
         const movies = results.Search;
         let cards = '';
-        movies.forEach(r => {
-            cards += `<div class="col-md-4 my-3">
-                        <div class="card">
-                            <img src="${r.Poster}" class="card-img-top">
-                            <div class="card-body">
-                            <h5 class="card-title">${r.Title}</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">${r.Year}</h6>
-                            <a href="#" class="btn btn-primary">Show Details</a>
-                            </div>
-                        </div>
-                      </div>`;
+        movies.forEach(m => {
+            cards += showCard(m);
         });
         $('.movie-container').html(cards);
+
+        /* ketika tombol detail di klik*/
+        $('.modal-detail').on('click', function (){
+            $.ajax({
+                url: 'http://www.omdbapi.com/?apikey=c4b6762c&i=' + $(this).data('imdbid'),
+                success : m => {
+                    
+                    const movieDetail = showmd(m);
+                    $('.modal-body').html(movieDetail);
+                
+                }, error: (e) => {
+                    console.log(e.responseText);
+                } 
+            });
+        }) 
+
     },
     error: (e) => {
         console.log(e.responseText);
     }
+}); 
 });
+
+
+
+
+
+function showCard(m){
+    return `<div class="col-md-4 my-3">
+             <div class="card">
+                <img src="${m.Poster}" class="card-img-top">
+                <div class="card-body">
+                <h5 class="card-title">${m.Title}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+                <a href="#" class="btn btn-info modal-detail" data-toggle="modal" data-target="#modalDetail" data-imdbid="${m.imdbID}">Show Details</a>
+                </div>
+                </div>
+            </div>`;
+}
+
+function showmd(m) {
+    return `<div class="container-fluid">
+            <div class="row">
+            <div class="col-md-3">
+                <img src="${m.Poster}" class="img-fluid">
+            </div>
+            <div class="col-md">
+                <ul class="list-group">
+                <li class="list-group-item"><h4>${m.Title}(${m.Year})</h4></li>
+                <li class="list-group-item"><strong>Genre :</strong> ${m.Genre}</li>
+                <li class="list-group-item"><strong>Director : </strong>${m.Director}</li>
+                <li class="list-group-item"><strong>Actors : </strong>${m.Actors}</li>
+                <li class="list-group-item"><strong>Language : </strong>${m.Language}</li>
+                <li class="list-group-item"><strong>Plot : </strong> <br> ${m.Plot}</li>
+                </ul>
+            </div>
+            </div>
+        </div>`;
+}
